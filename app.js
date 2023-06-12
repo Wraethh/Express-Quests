faf927781e2de1a2563d572dae44091188273a89
Guillaume Lamourec <parkerpool@live.fr>
2023-06-12 11:42:48 +0200

    updated with delete methods

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-
 const app = express();
-
 app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
@@ -14,15 +12,16 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+// --- Movies ---
+
 const movieHandlers = require("./movieHandlers");
 const { validateMovie } = require("./validators.js");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 // --- Users ---
 
@@ -32,10 +31,9 @@ const { hashPassword } = require("./auth.js");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-
 app.post("/api/users", validateUser, hashPassword, userHandlers.postUser);
-
 app.put("/api/users/:id", validateUser, hashPassword, userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
